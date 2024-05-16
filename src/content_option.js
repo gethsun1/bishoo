ae547717e67254e524df7a9701284888739619f6
quasar <gethsun09@gmail.com>
2024-05-16 14:43:57 +0300

    first commit

--- src/content_option.js
+++ src/content_option.js
@@ -38,71 +38,71 @@ const services = [{
 ];
 
 const dataportfolio = [{
-        img: "https://scontent.cdninstagram.com/v/t39.30808-6/434746304_18021163913116467_8336828430615568584_n.jpg?stp=dst-jpg_e35_p1080x1080&efg=eyJ2ZW5jb2RlX3RhZyI6ImltYWdlX3VybGdlbi4xNDQweDE4MDAuc2RyLmYzMDgwOCJ9&_nc_ht=scontent.cdninstagram.com&_nc_cat=103&_nc_ohc=W7Gs8UyjjMkQ7kNvgEUFTqy&edm=APs17CUAAAAA&ccb=7-5&ig_cache_key=MzMzNTEwMzIxOTA0MTE0NjgxNQ%3D%3D.2-ccb7-5&oh=00_AfDFfZAjhBr4SGr4OhumOv8jN5_ITMvoU8-W3R7EgCIvZQ&oe=663FECD9&_nc_sid=10d13b",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
+        img: "https://i.postimg.cc/Gp89LHQH/T1.jpg",
+        description: "Discover a world of sophistication ",
         link: "#",
     },
     {
-        img: "https://scontent.cdninstagram.com/v/t39.30808-6/434728485_18021162083116467_4069144259665198373_n.jpg?stp=dst-jpg_e35&efg=eyJ2ZW5jb2RlX3RhZyI6ImltYWdlX3VybGdlbi4xNDQweDE4MDAuc2RyLmYzMDgwOCJ9&_nc_ht=scontent.cdninstagram.com&_nc_cat=103&_nc_ohc=KzoOYQzCrUAQ7kNvgH2nWJq&edm=APs17CUAAAAA&ccb=7-5&ig_cache_key=MzMzNTA4NzkxNjg2OTcxNzY0NA%3D%3D.2-ccb7-5&oh=00_AfC1zXbL3A3AIFezqQOqdr4osVjXdSWLKTjKP0zXt34BoA&oe=663FE352&_nc_sid=10d13b",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
+        img: "https://i.postimg.cc/50v2wkvF/T2.jpg",
+        description: "Discover a world of sophistication ",
         link: "#",
     },
     {
-        img: "https://scontent.cdninstagram.com/v/t39.30808-6/434726313_18021162026116467_17280557116925205_n.jpg?stp=dst-jpg_e35&efg=eyJ2ZW5jb2RlX3RhZyI6ImltYWdlX3VybGdlbi4xNDQweDE4MDAuc2RyLmYzMDgwOCJ9&_nc_ht=scontent.cdninstagram.com&_nc_cat=103&_nc_ohc=_BOP_6OFjncQ7kNvgEF5o5x&edm=APs17CUAAAAA&ccb=7-5&ig_cache_key=MzMzNTA4NzUzNTY1NzU5Mzc1MA%3D%3D.2-ccb7-5&oh=00_AfCpUk3Ndq_xRnUIbWzjft0IDMqobSKgeCNTDTsWw1Njyg&oe=663FC4EB&_nc_sid=10d13b",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
+        img: "https://i.postimg.cc/4NW3GcQq/T3.jpg",
+        description: "Discover a world of sophistication ",
         link: "#",
     },
     {
-        img: "https://scontent.cdninstagram.com/v/t39.30808-6/434763441_18021162005116467_6167458378003396709_n.jpg?stp=dst-jpg_e35&efg=eyJ2ZW5jb2RlX3RhZyI6ImltYWdlX3VybGdlbi4xNDQweDE4MDAuc2RyLmYzMDgwOCJ9&_nc_ht=scontent.cdninstagram.com&_nc_cat=103&_nc_ohc=YvnJz625ff0Q7kNvgFYKxcc&edm=APs17CUAAAAA&ccb=7-5&ig_cache_key=MzMzNTA4NzM5MDYzNTMzNDg4OQ%3D%3D.2-ccb7-5&oh=00_AfBA0IKcZk8CA6ZcyfSASS6Dj0f7WNEa3Uze8uhG35jvQQ&oe=663FF204&_nc_sid=10d13b",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
+        img: "https://i.postimg.cc/PfRtFHfn/T4.jpg",
+        description: "Discover a world of sophistication ",
         link: "#",
     },
     {
-        img: "https://scontent.cdninstagram.com/v/t39.30808-6/434722458_18021161987116467_3969864584566304907_n.jpg?stp=dst-jpg_e35&efg=eyJ2ZW5jb2RlX3RhZyI6ImltYWdlX3VybGdlbi4xNDQweDE4MDAuc2RyLmYzMDgwOCJ9&_nc_ht=scontent.cdninstagram.com&_nc_cat=103&_nc_ohc=zKXn6pyDhjYQ7kNvgETjGpI&edm=APs17CUAAAAA&ccb=7-5&ig_cache_key=MzMzNTA4NzIzMjE1Nzg4NDU2NQ%3D%3D.2-ccb7-5&oh=00_AfDx2wvIpIL5Of-O2nhKtCtE7l-aAIn0ylRYhX0Sl3Xd0g&oe=663FD3A7&_nc_sid=10d13b",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
+        img: "https://i.postimg.cc/nzLJ0R6L/T5.jpg",
+        description: "Discover a world of sophistication ",
         link: "#",
     },
     {
-        img: "https://scontent.cdninstagram.com/v/t39.30808-6/434726764_18021161969116467_8556398368980864952_n.jpg?stp=dst-jpg_e35&efg=eyJ2ZW5jb2RlX3RhZyI6ImltYWdlX3VybGdlbi4xNDQweDE4MDAuc2RyLmYzMDgwOCJ9&_nc_ht=scontent.cdninstagram.com&_nc_cat=103&_nc_ohc=1zijOhl7bBoQ7kNvgFpxmnu&edm=APs17CUAAAAA&ccb=7-5&ig_cache_key=MzMzNTA4NzA2MjMyMTk5MTYwNg%3D%3D.2-ccb7-5&oh=00_AfDrts3AbqBnZufrEI3mXeog4vPEeVL1Hjk1lQDVPfFmmA&oe=663FE2EA&_nc_sid=10d13b",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
+        img: "https://i.postimg.cc/bvycwVYR/T6.jpg",
+        description: "Discover a world of sophistication ",
         link: "#",
     },
 
     {
-        img: "https://scontent.cdninstagram.com/v/t39.30808-6/434728607_18021161888116467_7239426441474552369_n.jpg?stp=dst-jpg_e35&efg=eyJ2ZW5jb2RlX3RhZyI6ImltYWdlX3VybGdlbi4xNDQweDE4MDAuc2RyLmYzMDgwOCJ9&_nc_ht=scontent.cdninstagram.com&_nc_cat=103&_nc_ohc=2oV21jXJO5MQ7kNvgE_qP5K&edm=APs17CUAAAAA&ccb=7-5&ig_cache_key=MzMzNTA4NjQ5OTM2MjUzOTU3MQ%3D%3D.2-ccb7-5&oh=00_AfBZ2DFzJK79IO80uVDHFA7pVDX1pHmqWcrpEGp2fAUpdQ&oe=663FC900&_nc_sid=10d13b",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
+        img: "https://i.postimg.cc/qvLBw3zj/T7.jpg",
+        description: "Discover a world of sophistication ",
         link: "#",
     },
     {
-        img: "https://scontent.cdninstagram.com/v/t39.30808-6/434760765_18021161513116467_1544772889092871860_n.jpg?stp=dst-jpg_e35&efg=eyJ2ZW5jb2RlX3RhZyI6ImltYWdlX3VybGdlbi4xNDQweDE4MDAuc2RyLmYzMDgwOCJ9&_nc_ht=scontent.cdninstagram.com&_nc_cat=103&_nc_ohc=67QTFxvCHv8Q7kNvgEDVs5O&edm=APs17CUAAAAA&ccb=7-5&ig_cache_key=MzMzNTA4MzMzMzM1MTEwNjc4Ng%3D%3D.2-ccb7-5&oh=00_AfAP2k5Qt5mTBteMyqBmTCr70tRRa4DdnGvhMJjvPtu4cg&oe=663FCD4C&_nc_sid=10d13b",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
+        img: "https://i.postimg.cc/yYTzCzsN/T8.jpg",
+        description: "Discover a world of sophistication ",
         link: "#",
     },
     {
-        img: "https://scontent.cdninstagram.com/v/t39.30808-6/434733419_18021161321116467_1764803641406344735_n.jpg?stp=dst-jpg_e35&efg=eyJ2ZW5jb2RlX3RhZyI6ImltYWdlX3VybGdlbi4xNDQweDE4MDAuc2RyLmYzMDgwOCJ9&_nc_ht=scontent.cdninstagram.com&_nc_cat=103&_nc_ohc=xrd043lOfYMQ7kNvgFVvoFR&edm=APs17CUAAAAA&ccb=7-5&ig_cache_key=MzMzNTA4MjUxNTk4MTcwMzEyMA%3D%3D.2-ccb7-5&oh=00_AfBL3iAj1K3XYZXSDLgfPMESTR2MM5XHP3netdadlAk9zA&oe=663FD748&_nc_sid=10d13b",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
+        img: "https://i.postimg.cc/LXPrycx2/T9.jpg",
+        description: "Discover a world of sophistication ",
         link: "#",
     },
     {
-        img: "https://scontent.cdninstagram.com/v/t39.30808-6/434730081_18021161276116467_8763850075327220437_n.jpg?stp=dst-jpg_e35&efg=eyJ2ZW5jb2RlX3RhZyI6ImltYWdlX3VybGdlbi4xNDQweDE4MDAuc2RyLmYzMDgwOCJ9&_nc_ht=scontent.cdninstagram.com&_nc_cat=103&_nc_ohc=yxafvOzGJP0Q7kNvgEAA_4L&edm=APs17CUAAAAA&ccb=7-5&ig_cache_key=MzMzNTA4MjExMTY0MjQ2NzI0NQ%3D%3D.2-ccb7-5&oh=00_AfDkq6WuXN2iC6Lz1f_7IVwT6ibQ2eAj8ZsUnMdwrw62ag&oe=663FE0E7&_nc_sid=10d13b",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
+        img: "https://i.postimg.cc/HLcKnGDX/T10.jpg",
+        description: "Discover a world of sophistication ",
         link: "#",
     },
     {
-        img: "https://scontent.cdninstagram.com/v/t39.30808-6/434739708_18021161240116467_1258461893162363044_n.jpg?stp=dst-jpg_e35&efg=eyJ2ZW5jb2RlX3RhZyI6ImltYWdlX3VybGdlbi4xNDQweDE4MDAuc2RyLmYzMDgwOCJ9&_nc_ht=scontent.cdninstagram.com&_nc_cat=103&_nc_ohc=dq0kHAySdm4Q7kNvgHv8qzX&edm=APs17CUAAAAA&ccb=7-5&ig_cache_key=MzMzNTA4MTk0MjI1MTMyMjM5OQ%3D%3D.2-ccb7-5&oh=00_AfApMOqm_A_1tG8hXNFRwzIkRQF9bLYnS1njH3oqDjHKAQ&oe=663FEBC3&_nc_sid=10d13b",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
+        img: "https://i.postimg.cc/vZjqpPgB/T11.jpg",
+        description: "Discover a world of sophistication ",
         link: "#",
     },
     {
-        img: "https://scontent.cdninstagram.com/v/t39.30808-6/434726734_18021161159116467_8770306243073856234_n.jpg?stp=dst-jpg_e35&efg=eyJ2ZW5jb2RlX3RhZyI6ImltYWdlX3VybGdlbi4xNDQweDE4MDAuc2RyLmYzMDgwOCJ9&_nc_ht=scontent.cdninstagram.com&_nc_cat=103&_nc_ohc=-RSHk-rECZkQ7kNvgHuUyYu&edm=APs17CUAAAAA&ccb=7-5&ig_cache_key=MzMzNTA4MTI2MDAxNDI0MDgwNg%3D%3D.2-ccb7-5&oh=00_AfD1FCvxbe4D22vugidQQVxrUZTfZ0keB4df6bku39wMDA&oe=663FD77D&_nc_sid=10d13b",
-        description: "The wisdom of life consists in the elimination of non-essentials.",
+        img: "https://i.postimg.cc/MKHYq78j/T12.jpg",
+        description: "Discover a world of sophistication ",
         link: "#",
     },
 ];
 
 const contactConfig = {
-    YOUR_EMAIL: "name@domain.com",
-    YOUR_FONE: "(555)123-4567",
+    YOUR_EMAIL: "bishoo@outfitters.co.ke",
+    YOUR_FONE: "(254)0708 385223",
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
     // creat an emailjs.com account 
     // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -47,15 +47,15 @@ export const Home = () => {
                       <div className="ring three"></div>
                     </div>
                   </Link>
-                  <Link to="/contact">
-                    <div id="button_h" className="ac_btn btn">
-                      Contact Me
-                      <div className="ring one"></div>
-                      <div className="ring two"></div>
-                      <div className="ring three"></div>
-                    </div>
+                  <Link to="https://wa.me/254708385223" target="_blank" rel="noopener noreferrer">
+                  <div id="button_h" className="ac_btn btn">
+                    Contact Me
+                    <div className="ring one"></div>
+                    <div className="ring two"></div>
+                    <div className="ring three"></div>
+                  </div>
                   </Link>
-                </div>
+               </div>
               </div>
             </div>
           </div>
