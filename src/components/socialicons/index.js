52eaf6e288777f37488e5dc226b072d72e343f5a
quasar <gethsun09@gmail.com>
2024-05-16 13:49:12 +0300

    first commit

---
+++ src/components/socialicons/index.js
@@ -0,0 +1,59 @@
+import React from "react";
+import "./style.css";
+
+import { FaInstagram,
+  FaFacebook,
+ } from "react-icons/fa";
+import { socialprofils } from "../../content_option";
+
+export const Socialicons = (params) => {
+  return (
+    <div className="stick_follow_icon">
+      <ul>
+        {socialprofils.twitter && (
+          <li>
+            <a href={socialprofils.twitter}>
+              <FaTwitter />
+            </a>
+          </li>
+        )}
+        {socialprofils.instagram && (
+          <li>
+            <a href={socialprofils.instagram}>
+              <FaInstagram />
+            </a>
+          </li>
+        )}
+        {socialprofils.facebook && (
+          <li>
+            <a href={socialprofils.facebook}>
+              <FaFacebook />
+            </a>
+          </li>
+        )}
+        {socialprofils.linkedin && (
+          <li>
+            <a href={socialprofils.linkedin}>
+              <FaLinkedin />
+            </a>
+          </li>
+        )}
+        {socialprofils.youtube && (
+          <li>
+            <a href={socialprofils.youtube}>
+              <FaYoutube />
+            </a>
+          </li>
+        )}
+        {socialprofils.twitch && (
+          <li>
+            <a href={socialprofils.twitch}>
+              <FaTwitch />
+            </a>
+          </li>
+        )}
+      </ul>
+      <p>Follow Me</p>
+    </div>
+  );
+};
